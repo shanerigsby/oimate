84dc482ca78a56745c96b7bf092d4eeed444d5da
shanerigsby <55967716+shanerigsby@users.noreply.github.com>
2024-05-02 12:10:47 -0500

    try to see if at latest will work

--- whatever.go
+++ whatever.go
@@ -3,5 +3,5 @@ package oimate
 import "fmt"
 
 func Oi() {
-	fmt.Println("Oi, mate! I don't want to have to make a new tag and clear modcache every time I make a change.")
+	fmt.Println("Oi, mate! 12:10pm")
 }
